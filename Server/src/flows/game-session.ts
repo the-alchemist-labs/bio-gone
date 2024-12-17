1685f73a384bebb4f86ccecd6b022da819ded38a
Sol Elan <solipod8@gmail.com>
2024-12-17 20:29:20 +0200

    Add log to join lobby

--- Server/src/flows/game-session.ts
+++ Server/src/flows/game-session.ts
@@ -12,6 +12,7 @@ export async function searchMatch(data: string, socket: Socket) {
         OpenRoom([lobby.shift()!, { socket, PlayerId }]);
     } else {
         lobby.push({ PlayerId, socket });
+        console.log(`Joined lobby - ${PlayerId}`);
     }
 }
 
